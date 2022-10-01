9d919b47728ce491d8aa259e465709d64c891cd6
Paulo Augusto <paulo-augusto12@outlook.com>
2022-10-01 13:50:11 -0300

    Finalizada tela de login - Iniciando tela do Jogo

---
+++ javascript/login.js
@@ -0,0 +1,22 @@
+const loginInput = document.querySelector('.loginInput')
+const button = document.querySelector('.loginButton')
+const form = document.querySelector('.loginForm')
+
+
+const validateInput = ({ target }) => {
+    if(target.value.length >2 ){
+        button.removeAttribute('disabled')
+        return;
+    } 
+        button.setAttribute('disabled' , '')
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('Player : ' , loginInput.value)
+    window.location = 'pages/game.html'
+}
+
+
+loginInput.addEventListener('input' , validateInput)
+form.addEventListener('submit' , handleSubmit)
